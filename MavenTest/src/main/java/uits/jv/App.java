960034a561dd1371da1765af0322c33327b75053
Ann <Anuta-first@mail.ru>
2015-10-27 20:24:25 +0200

    TestCommit

---
+++ MavenTest/src/main/java/uits/jv/App.java
@@ -0,0 +1,13 @@
+package uits.jv;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
